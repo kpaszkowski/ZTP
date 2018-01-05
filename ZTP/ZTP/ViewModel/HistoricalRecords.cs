a727a99d4dfa7f84fdd140fc2d4e2f847ac6e483
Hubert Kowalski <mnichwwy@gmail.com>
2018-01-05 01:59:29 +0100

    Added HistoricalRecords class

---
+++ ZTP/ZTP/ViewModel/HistoricalRecords.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZTP.ViewModel
+{
+    class HistoricalRecords
+    {
+        private HistoricalRecords instance;
+
+        public HistoricalRecords getInstance()
+        {
+            if (instance == null) instance = new HistoricalRecords();
+            return instance;
+        }
+
+        private String bestScorerName;
+        public String getBestScorerName() { return bestScorerName; }
+        public void setBestScorerName(String newName) { bestScorerName = newName; }
+
+        private int bestScorerGoals;
+        public int getBestScorerGoals() { return bestScorerGoals; }
+        public void setBestScorerGoals(int newGoals) { bestScorerGoals = newGoals; }
+
+        private String mostAppearancesName;
+        public String getMostAppearancesName() { return mostAppearancesName; }
+        public void setMostAppearancesName(String newName) { mostAppearancesName = newName; }
+
+        private int mostAppearancesAmount;
+        public int getMostAppearancesAmount() { return mostAppearancesAmount; }
+        public void setMostAppearancesAmount(int newAmount) { mostAppearancesAmount = newAmount; }
+
+        private String mostWinsTeam;
+        public String getMostWinsTeam() { return mostWinsTeam; }
+        public void setMostWinsTeam(String newTeam) { mostWinsTeam = newTeam; }
+
+        private int mostWinsAmount;
+        public int getMostWinsAmount() { return mostWinsAmount; }
+        public void setMostWinsAmount(int newAmount) { mostWinsAmount = newAmount; }
+    }
+}
