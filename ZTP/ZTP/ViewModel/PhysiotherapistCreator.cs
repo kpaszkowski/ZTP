da7cd8757b7ed22bf92f2d8481a7418fbc058e05
Hubert Kowalski <mnichwwy@gmail.com>
2018-01-05 02:28:15 +0100

    Added implementation of Factory Method for staff members creation

---
+++ ZTP/ZTP/ViewModel/PhysiotherapistCreator.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZTP.ViewModel
+{
+    class PhysiotherapistCreator : StaffCreator
+    {
+        override public StaffMember staffMemberFactoryMethod() { return new Physiotherapist(); }
+    }
+}
