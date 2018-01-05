7f7df4b91e0367b7b4fe38e596bdb727d0c86977
unknown <grzegorz_dziegielewski@onet.eu>
2018-01-05 01:39:04 +0100

    bindings

---
+++ Football/Football/TicketManager.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Football
+{
+    /// <summary>
+    /// Interaction logic for TicketManager.xaml
+    /// </summary>
+    public partial class TicketManager : Window
+    {
+        public TicketManager()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Cancel(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
